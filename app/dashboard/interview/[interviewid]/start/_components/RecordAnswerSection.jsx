764ba95c3e60772a1f048511a9936c335769ee90
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-09-10 22:20:41 +0530

    Record Answer Section half added with webcam and real questions

--- app/dashboard/interview/[interviewid]/start/_components/RecordAnswerSection.jsx
+++ app/dashboard/interview/[interviewid]/start/_components/RecordAnswerSection.jsx
@@ -1,8 +1,15 @@
+
+import Image from 'next/image'
 import React from 'react'
+import Webcam from 'react-webcam'
 
 function RecordAnswerSection() {
   return (
-    <div>RecordAnswerSection</div>
+    <div>
+        <Image src={''} width={200} height={200} className='absolute' />
+          <Webcam/>
+
+    </div>
   )
 }
 
